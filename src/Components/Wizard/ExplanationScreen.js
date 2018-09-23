da93e1291a58662bd2e01dfb0bb8f623db65de41
David Vos <vosdavid98@gmail.com>
2018-09-23 16:01:22 +0100

    wat heb je nodig

--- src/Components/Wizard/ExplanationScreen.js
+++ src/Components/Wizard/ExplanationScreen.js
@@ -9,22 +9,19 @@ class ExplanationScreen extends Component {
   render() {
     return(
       <div className='explanationSteps'>
-        <h1>Hoe werkt het?</h1>
+        <h1>Wat heb je nodig?</h1>
         <ul>
           <li>
             <img src={require('../../Assets/img/measure_tape.png')} alt='measure' className='explImg' />
-            <h2>Meet</h2>
-            <p>Voer de afmetingen in van je twee favoriete business overhemden (±10 min)</p>
+            <h2>Meetlint</h2>
           </li>
           <li>
-            <img src='https://uploads-ssl.webflow.com/5b3ce619a79cac271fc5c65a/5b3e3f62aa9722ca9bd6b14e_desktop.png' alt='order' className='explImg' />
-            <h2>Bestel</h2>
-            <p>Kies jouw nieuwe, best passende overhemd</p>
+            <img src={require('../../Assets/img/shirt_logo_double.png')} alt='order' className='explImg' />
+            <h2>Twee overhemden</h2>
           </li>
           <li>
-            <img src='https://uploads-ssl.webflow.com/5b3ce619a79cac271fc5c65a/5b3e3f62b90c1f6cd6ff3756_truck.png' alt='deliver' className='explImg' />
-            <h2>Ontvang</h2>
-            <p>Kies jouw nieuwe, best passende overhemd</p>
+            <img src={require('../../Assets/img/time-black.png')} alt='deliver' className='explImg' />
+            <h2>10 minuten de tijd</h2>
           </li>
         </ul>
         <div className='proceedButtons'>
